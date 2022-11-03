c5cbd0135e6b6f2b1527f84e90e8830b5fb13296
Romualdus Aldi Hermanto <romheraldi.dev@gmail.com>
2022-11-03 17:41:41 +0700

    Initial with web socket

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  messages: Message[] = [{ name: 'Romheraldi', message: 'halooo' }];
+  create(createMessageDto: CreateMessageDto) {
+    const message = { ...createMessageDto };
+    this.messages.push(message);
+
+    return message;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+}
